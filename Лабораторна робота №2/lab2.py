e9c3a614c450e9b07e201aa3be9dac5effad0e10
rmnstepaniuk <rmnstepaniuk@users.noreply.github.com>
2021-02-23 23:24:42 +0200

    Код програми до лабораторної роботи №2

--- Лабораторна робота №2/lab2.py
+++ Лабораторна робота №2/lab2.py
@@ -53,9 +53,9 @@ def homogeneity():
         return dispertion
 
     # Функції відгуку в точках експерименту
-    y1 = [random.random() * 10 + y_min for _ in range(m)]
-    y2 = [random.random() * 10 + y_min for _ in range(m)]
-    y3 = [random.random() * 10 + y_min for _ in range(m)]
+    y1 = [random.randint(0, 100) + y_min for _ in range(m)]
+    y2 = [random.randint(0, 100) + y_min for _ in range(m)]
+    y3 = [random.randint(0, 100) + y_min for _ in range(m)]
 
     # Дисперсія
     dispertion = [dispertion(y1), dispertion(y2), dispertion(y3)]
@@ -73,18 +73,9 @@ def homogeneity():
     print("Значення факторів у точках експерименту:")
     for line in matrix:
         print(line)
-    print(f"""  
-Функції відгуку:
-
-В першій точці: {y1}
-В другій точці: {y2}
-В третій точці: {y3}
-    """)
-    print(f"""
-Дисперсія в першій точці: {dispertion[0]};
-Дисперсія в другій точці: {dispertion[1]};
-Дисперсія в третій точці: {dispertion[2]};
-    """)
+    print(f"\nФункції відгуку:\nВ першій точці: {y1}\nВ другій точці: {y2}\nВ третій точці: {y3}")
+    print(f"\nСередні значення Y:\n{average(y1)}\n{average(y2)}\n{average(y3)}")
+    print(f"\nДисперсія: {dispertion}")
     y = [y1, y2, y3]
     normalisation(y)
 
@@ -103,26 +94,38 @@ def normalisation(y):
 
     a11 = (normalized_matrix[0][0] * average(y[0]) + normalized_matrix[1][0] * average(y[1]) + normalized_matrix[2][0] * average(y[2])) / 3
     a22 = (normalized_matrix[0][1] * average(y[0]) + normalized_matrix[1][1] * average(y[1]) + normalized_matrix[2][1] * average(y[2])) / 3
-    
-    b0_numerator = np.array([[my, mx1, mx2], [a11, a1, a2], [a22, a2, a3]])
-    b0_denominator = np.array([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
+
+    b0_numerator = np.array([[my, mx1, mx2], 
+                             [a11, a1, a2],
+                             [a22, a2, a3]])
+    b0_denominator = np.array([[1, mx1, mx2],
+                               [mx1, a1, a2],
+                               [mx2, a2, a3]])
 
     b0 = np.linalg.det(b0_numerator) / np.linalg.det(b0_denominator)
 
-    b1_numerator = np.array([[1, my, mx2], [mx1, a11, a2], [mx2, a22, a3]])
-    b1_denominator = np.array([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
+    b1_numerator = np.array([[1, my, mx2], 
+                             [mx1, a11, a2], 
+                             [mx2, a22, a3]])
+    b1_denominator = np.array([[1, mx1, mx2], 
+                               [mx1, a1, a2], 
+                               [mx2, a2, a3]])
 
     b1 = np.linalg.det(b1_numerator) / np.linalg.det(b1_denominator)
 
-    b2_numerator = np.array([[1, mx1, my], [mx1, a1, a11], [mx2, a2, a22]])
-    b2_denominator = np.array([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])
+    b2_numerator = np.array([[1, mx1, my],
+                             [mx1, a1, a11],
+                             [mx2, a2, a22]])
+    b2_denominator = np.array([[1, mx1, mx2],
+                               [mx1, a1, a2],
+                               [mx2, a2, a3]])
 
     b2 = np.linalg.det(b2_numerator) / np.linalg.det(b2_denominator)
 
     # Натуралізація коефіцієнтів
+
     delta_x1 = abs(x_max[0] - x_min[0]) / 2
     delta_x2 = abs(x_max[1] - x_min[1]) / 2
-
 
     x10 = (x_max[0] + x_min[0]) / 2
     x20 = (x_max[1] + x_min[1]) / 2
@@ -132,14 +135,24 @@ def normalisation(y):
     a2 = b2 / delta_x2
 
 ####################################### В И В Е Д Е Н Н Я   Р Е З У Л Ь Т А Т І В   У   К О Н С О Л Ь #########################################
-    print("Нормалізована матриця")
+    print("\nНормалізована матриця")
     for line in normalized_matrix:
         print(line)
-    print(f"\nΔx1 = {delta_x1}; Δx2 = {delta_x2}")
     print(f"\nb0 = {b0}; b1 = {b1}; b2 = {b2}")
-    print(f"\nНормоване рівняння регресії:\ny = {round(b0, 3)} + {round(b1, 3)}*x1 + {round(b2, 3)}*x2")
+    print(f"Нормоване рівняння регресії:\ny = {round(b0, 3)} + {round(b1, 3)}*x1 + {round(b2, 3)}*x2")
+    # Перевірка
+    print("Середні значення нормованих Y:")
+    for i in range(3):
+        print(f"{b0 + b1*normalized_matrix[i][0] + b2*normalized_matrix[i][1]}")
+    print("Значення збігаються зі значеннями Yj")
     print(f"\na0 = {a0}; a1 = {a1}; a2 = {a2}")
-    print(f"\nНатуралізоване рівняння регресії:\ny = {round(a0, 3)} + {round(a1, 3)}*x1 + {round(a2, 3)}*x2")
+    print(f"Натуралізоване рівняння регресії:\ny = {round(a0, 3)} + {round(a1, 3)}*x1 + {round(a2, 3)}*x2")
+    # Перевірка
+    print("\nСередні значення натуралізованих Y:")
+    for i in range(3):
+        print(f"{a0 + a1*matrix[i][0] + a2*matrix[i][1]}")
+    print("Значення збігаються зі значеннями Yj")
+    print("Отже, коефіцієнти рівняння регресії розраховані правильно")
 
 # Кількість експериментів, межі у і х, додатковий нульовий фактор
 m = 6
